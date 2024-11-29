0fd87a6ca51df936541a56da11401bbe991e095e
ImadBKZZ <ied.boukezzata@gmail.com>
2024-11-29 16:04:57 +0100

    Mission MS

---
+++ RequestManagementService/src/main/java/insa/fr/msa/RequestManagementService/RequestManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package insa.fr.msa.RequestManagementService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestManagementServiceApplication.class, args);
+	}
+
+}
